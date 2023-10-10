3913a92d9e15776acc89a06f6c8cc8efd54a5535
Cameron Hillman <cameron@hillman.id.au>
2023-10-10 20:29:40 +1100

    add support for Genshin Impact

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ const scriptProperties = PropertiesService.getScriptProperties();
 const COOKIE = scriptProperties.getProperty("COOKIE");
 const DISCORD_WEBHOOK = scriptProperties.getProperty("DISCORD_WEBHOOK");
 
-const ACT_ID = "e202303301540311";
-const BASE_URL = "https://sg-public-api.hoyolab.com/event/luna/os";
+const HSR_ACT_ID = "e202303301540311";
+const HSR_BASE_URL = "https://sg-public-api.hoyolab.com/event/luna/os";
+
+const GENSHIN_ACT_ID = "e202102251931481";
+const GENSHIN_BASE_URL = "https://sg-hk4e-api.hoyolab.com/event/sol";
 
 class Discord {
 	constructor (DISCORD_WEBHOOK) {
@@ -29,21 +32,21 @@ class Discord {
 				payload: JSON.stringify({
 					embeds: [
 						{
-							title: "Honkai: Star Rail Auto Check-in",
+							title: "Hoyolab Auto Check-in",
 							author: {
-								name: "Honkai: Star Rail",
-								icon_url: "https://i.imgur.com/o0hyhmw.png"
+								name: "Hoyolab",
+								icon_url: "https://i.imgur.com/cakGdFP.png"
 							},
 							description: data.message,
 							color: 0xBB0BB5,
 							timestamp: new Date(),
 							footer: {
-								text: "Honkai: Star Rail Auto Check-in"
+								text: "Hoyolab Auto Check-in"
 							}
 						}
 					],
-					username: "Honkai: Star Rail",
-					avatar_url: "https://i.imgur.com/o0hyhmw.png"
+					username: "Hoyolab",
+					avatar_url: "https://i.imgur.com/cakGdFP.png"
 				})
 			});
 
@@ -60,8 +63,8 @@ class Discord {
 			contentType: "application/json",
 			payload: JSON.stringify({
 				embeds: [embed],
-				username: "Honkai: Star Rail",
-				avatar_url: "https://i.imgur.com/o0hyhmw.png"
+				username: "Hoyolab",
+				avatar_url: "https://i.imgur.com/cakGdFP.png"
 			})
 		});
 
@@ -74,17 +77,17 @@ class Discord {
 
 	static generateEmbed (data = {}) {
 		return {
-			title: "Honkai: Star Rail Auto Check-in",
+			title: "Hoyolab Auto Check-in",
 			author: {
-				name: "Honkai: Star Rail",
-				icon_url: "https://i.imgur.com/o0hyhmw.png"
+				name: "Hoyolab",
+				icon_url: "https://i.imgur.com/cakGdFP.png"
 			},
 			description: `Today's reward: ${data.award.name} x${data.award.count}`
         + `\nTotal signed: ${data.signed}`,
 			color: 0xBB0BB5,
 			timestamp: new Date(),
 			footer: {
-				text: "Honkai: Star Rail Auto Check-in"
+				text: "Hoyolab Auto Check-in"
 			}
 		};
 	}
@@ -93,7 +96,7 @@ class Discord {
 class StarRail {
 	constructor (cookie) {
 		if (typeof cookie !== "string" || typeof cookie === "undefined") {
-			throw new Error("cookie must be a string");
+			throw new Error("[HSR] cookie must be a string");
 		}
 
 		this.cookie = cookie;
@@ -101,7 +104,7 @@ class StarRail {
 
 	static async sign (cookie) {
 		const payload = {
-			act_id: ACT_ID
+			act_id: HSR_ACT_ID
 		};
 
 		const options = {
@@ -113,15 +116,15 @@ class StarRail {
 			payload: JSON.stringify(payload)
 		};
 
-		const res = UrlFetchApp.fetch(`${BASE_URL}/sign`, options);
+		const res = UrlFetchApp.fetch(`${HSR_BASE_URL}/sign`, options);
 
 		if (res.getResponseCode() !== 200) {
-			throw new Error(`Sign HTTP error: ${res.getResponseCode()}`);
+			throw new Error(`[HSR] Sign HTTP error: ${res.getResponseCode()}`);
 		}
 
 		const body = JSON.parse(res.getContentText());
 		if (body.retcode !== 0 && body.message !== "OK") {
-			throw new Error(`Sign API error: ${body.message}`);
+			throw new Error(`[HSR] Sign API error: ${body.message}`);
 		}
 
 		return true;
@@ -135,11 +138,6 @@ class StarRail {
 			counter++;
 
 			const info = await StarRail.getInfo(cookie);
-			const awards = await StarRail.awards(cookie);
-			if (awards.length === 0) {
-				throw new Error("There's no awards to claim (?)");
-			}
-
 			const data = {
 				today: info.today,
 				total: info.total_sign_day,
@@ -149,9 +147,14 @@ class StarRail {
 
 			const discord = new Discord(DISCORD_WEBHOOK);
 			if (data.issigned) {
-				await discord.send({ message: `[Account ${counter}]: You've already checked in today, Trailblazer~` }, true);
-				console.log(`[Account ${counter}]: You've already checked in today, Trailblazer~`);
+				await discord.send({ message: `[HSR][Account ${counter}]: You've already checked in today, Trailblazer~` }, true);
+				console.log(`[HSR][Account ${counter}]: You've already checked in today, Trailblazer~`);
 				continue;
+			}
+
+      const awards = await StarRail.awards(cookie);
+			if (awards.length === 0) {
+				throw new Error("[HSR] There's no awards to claim (?)");
 			}
 
 			const totalSigned = data.total;
@@ -162,16 +165,16 @@ class StarRail {
 
 			const sign = await StarRail.sign(cookie);
 			if (sign) {
-				console.log(`[Account ${counter}]: Signed in successfully! You have signed in for ${data.total} days!`);
-				console.log(`[Account ${counter}]: You have received ${awardData.count}x ${awardData.name}!`);
+				console.log(`[HSR][Account ${counter}]: Signed in successfully! You have signed in for ${data.total + 1} days!`);
+				console.log(`[HSR][Account ${counter}]: You have received ${awardData.count}x ${awardData.name}!`);
 
 				if (!DISCORD_WEBHOOK || typeof DISCORD_WEBHOOK !== "string") {
-					console.log("No Discord webhook provided, skipping...");
+					console.log("[HSR] No Discord webhook provided, skipping...");
 					return true;
 				}
 
 				await discord.send({
-					signed: data.total,
+					signed: data.total + 1,
 					award: awardData
 				});
 			}
@@ -188,15 +191,15 @@ class StarRail {
 			method: "GET"
 		};
 
-		const res = UrlFetchApp.fetch(`${BASE_URL}/info?act_id=${ACT_ID}`, options);
+		const res = UrlFetchApp.fetch(`${HSR_BASE_URL}/info?act_id=${HSR_ACT_ID}`, options);
 
 		if (res.getResponseCode() !== 200) {
-			throw new Error(`Info HTTP error: ${res.getResponseCode()}`);
+			throw new Error(`[HSR] Info HTTP error: ${res.getResponseCode()}`);
 		}
 
 		const body = JSON.parse(res.getContentText());
 		if (body.retcode !== 0 && body.message !== "OK") {
-			throw new Error(`Info API error: ${body.message}`);
+			throw new Error(`[HSR] Info API error: ${body.message}`);
 		}
 
 		return body.data;
@@ -212,16 +215,162 @@ class StarRail {
 			method: "GET"
 		};
 
-		const res = UrlFetchApp.fetch(`${BASE_URL}/home?act_id=${ACT_ID}`, options);
+		const res = UrlFetchApp.fetch(`${HSR_BASE_URL}/home?act_id=${HSR_ACT_ID}`, options);
 
 		if (res.getResponseCode() !== 200) {
-			throw new Error(`HTTP error: ${res.getResponseCode()}`);
+			throw new Error(`[HSR] HTTP error: ${res.getResponseCode()}`);
 		}
 
 		const body = JSON.parse(res.getContentText());
 
 		if (body.retcode !== 0 && body.message !== "OK") {
-			throw new Error(`API error: ${body.message}`);
+			throw new Error(`[HSR] API error: ${body.message}`);
+		}
+
+		return body.data.awards;
+	}
+
+	get parseCookies () {
+		return this.cookie.split("#");
+	}
+
+	static get userAgent () {
+		return "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36";
+	}
+}
+
+class Genshin {
+	constructor (cookie) {
+		if (typeof cookie !== "string" || typeof cookie === "undefined") {
+			throw new Error("[Genshin] cookie must be a string");
+		}
+
+		this.cookie = cookie;
+	}
+
+	static async sign (cookie) {
+		const payload = {
+			act_id: GENSHIN_ACT_ID
+		};
+
+		const options = {
+			method: "POST",
+			headers: {
+				"User-Agent": Genshin.userAgent,
+				Cookie: cookie
+			},
+			payload: JSON.stringify(payload)
+		};
+
+		const res = UrlFetchApp.fetch(`${GENSHIN_BASE_URL}/sign`, options);
+
+		if (res.getResponseCode() !== 200) {
+			throw new Error(`[Genshin] Sign HTTP error: ${res.getResponseCode()}`);
+		}
+
+		const body = JSON.parse(res.getContentText());
+		if (body.retcode !== 0 && body.message !== "OK") {
+			throw new Error(`[Genshin] Sign API error: ${body.message}`);
+		}
+
+		return true;
+	}
+
+	async run () {
+		const cookies = this.parseCookies;
+
+		let counter = 0;
+		for (const cookie of cookies) {
+			counter++;
+
+			const info = await Genshin.getInfo(cookie);
+			const data = {
+				today: info.today,
+				total: info.total_sign_day,
+				issigned: info.is_sign,
+				missed: info.sign_cnt_missed
+			};
+
+			const discord = new Discord(DISCORD_WEBHOOK);
+			if (data.issigned) {
+				await discord.send({ message: `[Genshin][Account ${counter}]: You've already checked in today, Traveler~` }, true);
+				console.log(`[Genshin][Account ${counter}]: You've already checked in today, Traveler~`);
+				continue;
+			}
+
+      const awards = await Genshin.awards(cookie);
+			if (awards.length === 0) {
+				throw new Error("[Genshin] There's no awards to claim (?)");
+			}
+
+			const totalSigned = data.total;
+			const awardData = {
+				name: awards[totalSigned].name,
+				count: awards[totalSigned].cnt
+			};
+
+			const sign = await Genshin.sign(cookie);
+			if (sign) {
+				console.log(`[Genshin][Account ${counter}]: Signed in successfully! You have signed in for ${data.total + 1} days!`);
+				console.log(`[Genshin][Account ${counter}]: You have received ${awardData.count}x ${awardData.name}!`);
+
+				if (!DISCORD_WEBHOOK || typeof DISCORD_WEBHOOK !== "string") {
+					console.log("[Genshin] No Discord webhook provided, skipping...");
+					return true;
+				}
+
+				await discord.send({
+					signed: data.total + 1,
+					award: awardData
+				});
+			}
+		}
+	}
+
+	static async getInfo (cookie) {
+		const options = {
+			headers: {
+				"User-Agent": Genshin.userAgent,
+				Cookie: cookie
+			},
+			muteHttpExceptions: true,
+			method: "GET"
+		};
+
+		const res = UrlFetchApp.fetch(`${GENSHIN_BASE_URL}/info?act_id=${GENSHIN_ACT_ID}`, options);
+
+		if (res.getResponseCode() !== 200) {
+			throw new Error(`[Genshin] Info HTTP error: ${res.getResponseCode()}`);
+		}
+
+		const body = JSON.parse(res.getContentText());
+		if (body.retcode !== 0 && body.message !== "OK") {
+			throw new Error(`[Genshin] Info API error: ${body.message}`);
+		}
+
+		return body.data;
+	}
+
+	static async awards (cookie) {
+		const options = {
+			headers: {
+				"User-Agent": Genshin.userAgent,
+				Cookie: cookie
+			},
+			muteHttpExceptions: true,
+			method: "GET"
+		};
+
+		const res = UrlFetchApp.fetch(`${GENSHIN_BASE_URL}/home?act_id=${GENSHIN_ACT_ID}`, options);
+
+		if (res.getResponseCode() !== 200) {
+			throw new Error(`[Genshin] HTTP error: ${res.getResponseCode()}`);
+		}
+
+		const body = JSON.parse(res.getContentText());
+
+		if (body.retcode !== 0 && body.message !== "OK") {
+			throw new Error(`[Genshin] API error: ${body.message}`);
 		}
 
 		return body.data.awards;
@@ -239,6 +388,11 @@ class StarRail {
 function run () {
 	const starRail = new StarRail(COOKIE);
 	starRail.run()
+		.catch((e) => {
+			console.error(e);
+		});
+  const genshin = new Genshin(COOKIE);
+	genshin.run()
 		.catch((e) => {
 			console.error(e);
 		});
